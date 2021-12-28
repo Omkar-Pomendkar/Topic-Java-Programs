7432f858ef64fcd49beafd9358a0adb154d25cc8
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2021-12-28 10:06:51 +0530

    One Dimensional Array

--- Arrays/src/Arrays1.java
+++ Arrays/src/Arrays1.java
@@ -1,2 +1,34 @@
 public class Arrays1 {
+    public static void main(String[] args) {
+        System.out.println("What is Array");
+
+        /*
+                Array is a dataStructure collection of similar (int) data/Objects
+         */
+
+//        Syntax of Array
+//        datatype array_name [] = new datatype[size];
+
+        // Array Declaration
+//            int arr[] ;
+
+        // Array Initialization
+//            arr[] = {10,20,30,40,50};
+//            index =   0  1  2  3  4
+
+        // Array Creation
+        int [] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 12;
+        arr[2] = 13;
+        arr[3] = 14;
+        arr[4] = 15;
+        System.out.println(arr[0]);
+
+        //Printing Array using For Loop
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    }
 }
