a1ddfadc55c8c0b1ba9e169ce53ff53e570dbf4e
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2021-12-29 11:25:07 +0530

    Searching in String

---
+++ Search/src/SearchinString.java
@@ -0,0 +1,22 @@
+public class SearchinString {
+    public static void main(String[] args) {
+        String name = "omkar";
+        char target = 'p';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String name,char target)
+    {
+       if(name.length() == 0)
+       {
+           return false;
+       }
+        for (int i = 0; i < name.length(); i++) {
+            if(target == name.charAt(i));
+            {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
