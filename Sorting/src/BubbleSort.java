fedcb24fdb8d87f51e7baddd52550a6fcca279d6
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-02 22:50:42 +0530

    BubbleSort

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] arr ={5,4,3,2,1};
+        int [] arr ={1,2,3,4,5};
         bubblesort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void bubblesort(int[] arr) {
+        boolean swapped;
         //run the steps n times
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             //for each steps, max item will come at last respective index
             for (int j = 1; j < arr.length-i ; j++) {
                 //swap if the item is smaller then previous
@@ -18,8 +20,16 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
                 
+            }
+            //if you did not swap for particular value of i,it means array is sorted hence stop the loop
+
+            if(!swapped)
+            {
+
+                break;
             }
 
         }
