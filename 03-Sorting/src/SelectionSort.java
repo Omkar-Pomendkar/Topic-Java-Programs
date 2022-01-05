012a462e9096f750be9ed3484dedab277eecef48
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-05 21:59:14 +0530

    SelectionSort Program

--- 03-Sorting/src/SelectionSort.java
+++ 03-Sorting/src/SelectionSort.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int [] arr= {4,5,3,2,1};
-        selectionsort(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void selectionsort(int[] arr) {
+    static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             //find the maximum item in the array & swap with correct index
             int last = arr.length-i-1;
@@ -26,7 +26,7 @@ public class SelectionSort {
      static int getMaxIndex(int[] arr, int start, int end) {
         int max = start;
 
-         for (int i = start; i < end; i++) {
+         for (int i = start; i <= end; i++) {
              if(arr[max] < arr[i])
              {
                  max = i;
