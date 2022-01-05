92de623baadcd9a3996fa177c9aee58519625034
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-05 21:58:06 +0530

    LinearSearch

--- src/com/omkar/LinearSearch.java
+++ src/com/omkar/LinearSearch.java
@@ -5,11 +5,11 @@ public class LinearSearch {
         System.out.println("Linear Search");
         int[] arr = {1,2,3,4,5,6,7,8};
         int target = 5;
-        int ans = linearsearch(arr,target);
+        int ans = linearSearch(arr,target);
         System.out.println(ans);
     }
 
-    static int linearsearch(int[] arr,int target) {
+    static int linearSearch(int[] arr,int target) {
         if(arr.length == 0)
         {
             return -1;
