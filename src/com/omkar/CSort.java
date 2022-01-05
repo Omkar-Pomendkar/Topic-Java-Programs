d106b0557a170f655e7af6fcec94b623e8cd9c8f
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-05 22:00:43 +0530

    Cyclic Sort

---
+++ src/com/omkar/CSort.java
@@ -0,0 +1,36 @@
+package com.omkar;
+
+import java.util.Arrays;
+
+public class CSort {
+    public static void main(String[] args) {
+        System.out.println("Practicing Cyclic Sort");
+        int[] arr = {4,3,5,2,1};
+        CyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void CyclicSort(int[] arr)
+    {
+        int i = 0;
+        while(i< arr.length)
+        {
+            int correct = arr[i]-1;
+            if(arr[i] != arr[correct])
+            {
+                swap(arr,i,correct);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+    }
+
+    static void swap(int[] arr,int first,int second)
+    {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
